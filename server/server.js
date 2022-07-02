c0148be3ef11d8a9e4f2b513569686f7f71f4ada
Timothée Binet <timobinet@yahoo.fr>
2022-07-02 15:33:07 +0200

    init project

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const cors = require('cors')
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+
+const PORT = process.env.PORT || 4000
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+app.use(cors())
+
+server.listen(PORT, ()=>console.log(`Listening on port ${PORT}`))
